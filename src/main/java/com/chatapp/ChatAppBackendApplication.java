dfd382180dd3be8726475ee08bb196b220484381
Yash Lodam <yashlodam03@gmail.com>
2026-08-10 08:47:58 +0530

    chat app backend intialize

---
+++ src/main/java/com/chatapp/ChatAppBackendApplication.java
@@ -0,0 +1,14 @@
+package com.chatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatAppBackendApplication.class, args);
+		System.out.print("Sucess");
+	}
+
+}
